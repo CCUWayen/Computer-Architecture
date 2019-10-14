338f4b7c031433a80d41b9c1c1a1452a3f2f85dc
CCUWayen <different3101@gmail.com>
2019-10-14 14:08:47 +0800

    assignment 1

--- assignment1/bit_reverse.s
+++ assignment1/bit_reverse.s
@@ -17,42 +17,43 @@ main:
     ecall
 
 bitreverse:
-	   lw t3,shift1
-      and t1,a0,t3
-      srli t1,t1,16
-      srli t3,t3,16
-      and t2,a0,t3
-      slli t2,t2,16
-      or t0,t1,t2
-      lw t3,shift2
-      and t1,t0,t3
-      srli t1,t1,8
-      srli t3,t3,8
-      and t2,t0,t3
-      slli t2,t2,8
-      or t0,t1,t2
-		 lw t3,shift3
-      and t1,t0,t3
-      srli t1,t1,4
-      srli t3,t3,4
-      and t2,t0,t3
-      slli t2,t2,4
-      or t0,t1,t2
-      lw t3,shift4
-      and t1,t0,t3
-      srli t1,t1,2
-      srli t3,t3,2
-      and t2,t0,t3
-      slli t2,t2,2
-      or t0,t1,t2
-		 lw t3,shift5
-      and t1,t0,t3
-      srli t1,t1,1
-      srli t3,t3,1
-      and t2,t0,t3
-      slli t2,t2,1
-      or t0,t1,t2
-      mv a0,t0
+    lw t3,shift1
+    and t1,a0,t3
+    srli t1,t1,16
+    srli t3,t3,16
+    and t2,a0,t3
+    slli t2,t2,16
+    or t0,t1,t2
+    lw t3,shift2
+    and t1,t0,t3
+    srli t1,t1,8
+    srli t3,t3,8
+    and t2,t0,t3
+    slli t2,t2,8
+    or t0,t1,t2
+    lw t3,shift3
+    and t1,t0,t3
+    srli t1,t1,4
+    srli t3,t3,4
+    and t2,t0,t3
+    slli t2,t2,4
+    or t0,t1,t2
+    lw t3,shift4
+    and t1,t0,t3
+    srli t1,t1,2
+    srli t3,t3,2
+    and t2,t0,t3
+    slli t2,t2,2
+    or t0,t1,t2
+    lw t3,shift5
+    and t1,t0,t3
+    srli t1,t1,1
+    srli t3,t3,1
+    and t2,t0,t3
+    slli t2,t2,1
+    or t0,t1,t2
+    mv a0,t0
+    ret
 print:
     mv t0,a0
     li a0,1 
@@ -63,3 +64,4 @@ print:
     mv a1,t0
     li a0,1
     ecall
+    ret
