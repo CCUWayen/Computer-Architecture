0c0cab79321d6fa516319d445538a5b9fc4496cf
CCUWayen <different3101@gmail.com>
2019-10-30 00:17:10 +0800

    finish

---
+++ assignment2/test.c
@@ -0,0 +1,35 @@
+void main(){
+	int mul1 = 10000;
+    	int mul2 = -200;
+    	int result = 0 ;
+    	int i ;
+	for(i=0;i<32;i++)
+    	{
+        	if((mul2 >> i) & 0x1)
+            		result = result + (mul1 << i);              
+    	}
+	printInt(result);
+}
+
+void printInt(int result){
+	volatile char* tx = (volatile char*) 0x40002000;
+ 	int dict[9] ={1,10,100,1000,10000,100000,1000000,10000000,100000000};
+ 	int mx = 900000000,i,j,isprint=0;
+	if(result >> 31){
+		*tx = '-';
+		result = -result;
+	} 
+	for(i=8;i>=0;i=i-1){
+		int print = -1;
+		for(j=9;j>=1;j--){
+			if ( result >= mx){
+				isprint =1;
+				print = j;
+				result = result - mx;
+			}
+			mx = (j!=1)? (mx - dict[i]):(mx-dict[i-1]);
+		}
+		if(print!=-1) *tx = (print+48);
+		else if (isprint) *tx = 48;
+	}
+}
